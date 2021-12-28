0957b7ae9f8ef7efebd7b790d9c8c5545f529d71
salaadas <carbonfibersweeper@gmail.com>
2021-12-28 18:33:45 +0700

    add styles

--- src/App.js
+++ src/App.js
@@ -2,22 +2,43 @@ import './App.css';
 import DisplayPokemon from './components/DisplayPokemon';
 import React, { useState } from 'react';
 import SearchBar from './components/SearchBar';
+import useWindowDimensions from './hooks/useWindowDimensions';
+
+const Wrapper = ({ children }) => {
+  return (
+    <div
+      id="wrapper"
+      style={{
+        overflow: 'hidden',
+        position: 'relative',
+      }}
+    >
+      {children}
+    </div>
+  );
+};
 
 function App() {
   const [state, setState] = useState({ loading: false, error: '', data: null });
+  const { width, height } = useWindowDimensions();
 
   return (
-    <div
-      style={{
-        MozUserSelect: 'none',
-        WebkitUserSelect: 'none',
-        msUserSelect: 'none',
-        userSelect: 'none',
-      }}
-    >
-      <SearchBar loading={state.loading} setState={setState} />
-      <DisplayPokemon state={state} />;
-    </div>
+    <Wrapper>
+      <div
+        style={{
+          MozUserSelect: 'none',
+          WebkitUserSelect: 'none',
+          msUserSelect: 'none',
+          userSelect: 'none',
+          backgroundColor: '#90aaf5',
+          width,
+          height,
+        }}
+      >
+        <SearchBar loading={state.loading} setState={setState} />
+        <DisplayPokemon state={state} />;
+      </div>
+    </Wrapper>
   );
 }
 
---
+++ src/hooks/useWindowDimensions.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+
+const getWindowDimensions = () => {
+  const { innerHeight: height, innerWidth: width } = window;
+  return {
+    width,
+    height,
+  };
+};
+
+const useWindowDimensions = () => {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimensions(getWindowDimensions);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return windowDimensions;
+};
+
+export default useWindowDimensions;
